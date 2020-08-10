e72181398c2688f6344393d1abd23703314cf6fe
paatryk01 <paatryk01@yahoo.pl>
2020-08-10 19:43:16 +0200

    empty branch

--- src/Enums.ts
+++
@@ -1,9 +0,0 @@
-enum States {
-  Start = "Good Luck! 🍀",
-  Lost = "You lost, try again! 💥",
-  Won = "You are a winner! 🏆",
-  NiceTry = "Nice try...",
-  SoClose = "So close..."
-}
-
-export default States;
