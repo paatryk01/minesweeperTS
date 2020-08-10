5026786776c1f1e7076ab8a62e10caac8956ebfb
paatryk01 <paatryk01@yahoo.pl>
2020-08-10 19:46:08 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>MineSweeper</h1>
+    <h3>Marked fields: {{ flags }} 🚩 / {{ bombsAmount }} 💣</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  get bombsAmount(): number {
+    const { bombsAmount } = this.$store.state;
+    return bombsAmount;
+  }
+
+  get flags(): number {
+    const { flags } = this.$store.state;
+    return flags;
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 40px;
+}
+h1,
+h3 {
+  text-align: center;
+}
+</style>
